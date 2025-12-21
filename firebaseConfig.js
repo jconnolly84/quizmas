cc3b3cc244ec18ff81d87aec6b305a226a7ab0a8
jconnolly84 <75783119+jconnolly84@users.noreply.github.com>
2025-12-21 14:51:41 +0000

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBrpucgCneGL2XFgWfKGUrYAZYQRr0apBc",
+  authDomain: "quizmas-73106.firebaseapp.com",
+  projectId: "quizmas-73106",
+  storageBucket: "quizmas-73106.firebasestorage.app",
+  messagingSenderId: "924265531416",
+  appId: "1:924265531416:web:23b4c9c8f03c13b04c1b6d"
+};
